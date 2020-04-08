48ea9abd69c7f0a370040239c449c5fe4552d634
Paweł Garapich <garmonek@gmail.com>
2020-04-08 02:51:30 +0200

    Add webpack. Add yarn. Remove cloudflare dependencies.

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,7 +6,18 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
+const $ = require('jquery');
+global.$ = global.jQuery = $;
+
 import '../css/app.css';
-import $ from 'jquery';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+import 'select2/dist/js/select2.full.min';
+import 'select2/dist/css/select2.min.css';
+
+import 'javascript-flex-images/flex-images.css';
+import flexImages from 'javascript-flex-images';
+new flexImages({ selector: '.flex-images', rowHeight: 140 });
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
